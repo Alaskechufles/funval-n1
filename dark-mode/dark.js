40887fcf6da155134f1e7c55b22b27569f57e384
DiegoHC <alask27echufles@gmail.com>
2025-04-10 15:57:17 -0500

    prueba dark mode

--- dark-mode/dark.js
+++ dark-mode/dark.js
@@ -1,6 +1,11 @@
-let darkButton = document.querySelector("#dark-button");
-darkButton.addEventListener("click", changeDarkmode);
+let buttonDark = document.querySelector("#dark");
 
-function changeDarkmode() {
+buttonDark.addEventListener("mouseup", patito);
+
+/* la función "patito" tiene la instrucción de cambiar la clase de mi etiqueta html y tambien hacer un "cuac" al hacerlo */
+
+function patito() {
   document.documentElement.classList.toggle("dark");
+  console.log("cuac");
+  console.log("");
 }
